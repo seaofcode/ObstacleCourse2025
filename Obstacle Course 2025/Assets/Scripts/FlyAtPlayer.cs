c0f13c425d284f62648081de046d4d3bb86b50b1
Matthew J Pearson <seaofcode@gmail.com>
2025-02-25 12:24:25 -0600

    Fly at Player
    Obstacle Hazard Flying at Player, Destroy Object, Trigger Projectile

---
+++ Obstacle Course 2025/Assets/Scripts/FlyAtPlayer.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class FlyAtPlayer : MonoBehaviour
+{
+    [SerializeField] Transform player;
+    [SerializeField] float moveSpeed;
+
+    Vector3 playerPosition;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        playerPosition = player.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveToPlayer();
+        DestroyWhenReached();
+    }
+
+    void MoveToPlayer()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, playerPosition, Time.deltaTime * moveSpeed);
+    }
+
+    void DestroyWhenReached()
+    {
+        if (transform.position == playerPosition)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
